14a90d70d4e62b23dabd51e16855b274cd3265c6
AshwinMyJobs <myjobsash@gmail.com>
2025-06-24 15:16:32 +0530

    Create PrefixSumArray.java
    Daily on program  push.

---
+++ scaler-dsa/src/main/java/dsa/arraya/PrefixSumArray.java
@@ -0,0 +1,26 @@
+package dsa.arraya;
+
+import java.util.Arrays;
+
+public class PrefixSumArray {
+
+    public static void main(String[] args) {
+        int[] inputArray = {1,2,3,4,5,6,7,8,9,10};
+        Arrays.stream(inputArray).forEach(System.out::print);
+        System.out.println();
+        createPrefixSumArray(inputArray);
+        System.out.println();
+    }
+
+    private static void createPrefixSumArray(int[] A){
+
+        int[] preFixSum = new int[A.length];
+
+        preFixSum[0] = A[0];
+
+        for (int i = 1; i < A.length; i++) {
+            preFixSum[i] = preFixSum[i-1] + A[i];
+        }
+        Arrays.stream(preFixSum).forEach(i->System.out.print(i + " "));
+    }
+}
